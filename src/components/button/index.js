4ef0394343349ed1cf9f448ab2a997a3e85ac31e
Jules Forrest <julesforrest@gmail.com>
2020-03-17 21:22:34 -0700

    better button?

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import classnames from "classnames";
 import { Link } from "gatsby";
+import PropTypes from "prop-types";
 import styles from "./index.module.scss";
 
-const Button = ({ children, type, href, to, onClick, fullWidth, disabled }) => {
-  const buttonStyles = classnames(
-    styles.main,
-    styles.fullWidth && fullWidth,
-    styles.disabled && disabled
-  );
+const Button = ({ children, type, href, to, onClick, width, size }) => {
+  const buttonStyles = classnames({
+    [styles.main]: true,
+    [styles.mainWidthFull]: width === "full",
+    [styles.mainWidthAuto]: width === "auto",
+    [styles.mainSizeRegular]: size === "regular"
+  });
+  const childStyles = classnames({
+    [styles.children]: true,
+    [styles.childrenSizeRegular]: size === "regular"
+  });
   const El = href ? "a" : to ? Link : "button";
   return (
     <El
@@ -17,11 +23,15 @@ const Button = ({ children, type, href, to, onClick, fullWidth, disabled }) => {
       href={href}
       to={to}
       onClick={onClick}
-      fullWidth={fullWidth || true}
     >
-      <span className={styles.children}>{children}</span>
+      <span className={childStyles}>{children}</span>
     </El>
   );
+};
+
+Button.defaultProps = {
+  width: "full",
+  size: "regular"
 };
 
 export default Button;
