685bffb6f1c8cb17129fba61b7410ca5e1fb87d5
Daniel O'Connor <daniel@danoc.me>
2022-03-19 12:07:21 -0400

    Implement localhost cache for Twitter profile fetching

--- utilities/get-twitter-profiles/index.js
+++ utilities/get-twitter-profiles/index.js
@@ -3,27 +3,42 @@ import { chunk, flatten } from "lodash-es";
 import addDescriptionLinks from "./add-description-links";
 import getTags from "./get-tags";
 
+let localhostCache;
+
 export default async function getTwitterProfiles(twitterAccountId) {
-  const client = new Twitter({
-    consumer_key: process.env.WWD_TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.WWD_TWITTER_CONSUMER_SECRET,
-    bearer_token: process.env.WWD_TWITTER_BEARER_TOKEN,
-  });
+  let chunkedDesigners;
 
-  const followingList = await client.get("friends/ids", {
-    user_id: twitterAccountId,
-    stringify_ids: true,
-  });
+  // We don't want to fetch from Twitter too often when developing locally
+  // becuase of rate limiting. We store the result of the first API requests
+  // so that we only do it once during `next dev`.
+  if (process.env.NODE_ENV === "development" && localhostCache) {
+    chunkedDesigners = localhostCache;
+  } else {
+    const client = new Twitter({
+      consumer_key: process.env.WWD_TWITTER_CONSUMER_KEY,
+      consumer_secret: process.env.WWD_TWITTER_CONSUMER_SECRET,
+      bearer_token: process.env.WWD_TWITTER_BEARER_TOKEN,
+    });
 
-  const chunkedDesigners = await Promise.all(
-    chunk(followingList.ids, 100).map(async (chunk) => {
-      const results = await client.get("users/lookup", {
-        user_id: chunk.join(","),
-      });
+    const followingList = await client.get("friends/ids", {
+      user_id: twitterAccountId,
+      stringify_ids: true,
+    });
 
-      return results;
-    })
-  );
+    chunkedDesigners = await Promise.all(
+      chunk(followingList.ids, 100).map(async (chunk) => {
+        const results = await client.get("users/lookup", {
+          user_id: chunk.join(","),
+        });
+
+        return results;
+      })
+    );
+
+    if (process.env.NODE_ENV === "development") {
+      localhostCache = chunkedDesigners;
+    }
+  }
 
   const profiles = flatten(chunkedDesigners);
 
